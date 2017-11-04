829e770ae331db664d3db8e2d360271e7b6b5dae
Stefánia Péter <lullaby@schatten.hu>
2017-11-04 19:32:53 +0100

    Twitter card infos added.

--- head.php
+++ head.php
@@ -5,6 +5,15 @@
     <title>Inktober project</title>
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon.png">
+
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@cat_a_flame">
+<meta name="twitter:creator" content="@cat_a_flame">
+<meta name="twitter:url" content="https://inktober.schatten.hu">
+<meta name="twitter:title" content="Stefanie's inktober project">
+<meta name="twitter:description" content="An amateur artist's drawings, mostly made for her own pleasure.">
+<meta name="twitter:image" content="/img/inktober_logo.png">
+
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto+Condensed:400,700|Roboto+Slab:400,700&amp;subset=latin-ext" rel="stylesheet">
     <link href="/dist/css/main.css" rel="stylesheet">
     <script src="/dist/js/main.js"></script>
