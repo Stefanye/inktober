e3226404ed690d0ac06599cbc9ec1aaea8042ac3
Stefánia Péter <lullaby@schatten.hu>
2017-11-04 19:36:38 +0100

    Meta tags updated.

--- head.php
+++ head.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
-<html lang="hu">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Inktober project</title>
+    <title>Inktober project by Stefanie</title>
     <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="description" content="List of my inktober drawings">
+    <meta name="keywords" content="Inktober, Art, Drawing, Project">
+    <meta name="author" content="Stefánia Péter">
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon.png">
-
-<meta name="twitter:card" content="summary">
-<meta name="twitter:site" content="@cat_a_flame">
-<meta name="twitter:creator" content="@cat_a_flame">
-<meta name="twitter:url" content="https://inktober.schatten.hu">
-<meta name="twitter:title" content="Stefanie's inktober project">
-<meta name="twitter:description" content="An amateur artist's drawings, mostly made for her own pleasure.">
-<meta name="twitter:image" content="/img/inktober_logo.png">
-
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@cat_a_flame">
+    <meta name="twitter:creator" content="@cat_a_flame">
+    <meta name="twitter:url" content="https://inktober.schatten.hu">
+    <meta name="twitter:title" content="Stefanie's inktober project">
+    <meta name="twitter:description" content="An amateur artist's drawings, mostly made for her own pleasure.">
+    <meta name="twitter:image" content="/img/inktober_logo.png">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto+Condensed:400,700|Roboto+Slab:400,700&amp;subset=latin-ext" rel="stylesheet">
     <link href="/dist/css/main.css" rel="stylesheet">
     <script src="/dist/js/main.js"></script>
